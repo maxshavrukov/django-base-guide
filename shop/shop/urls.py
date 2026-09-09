5bf8c8255ed0d5337b6c43784ebd547a92f4b4ce
max.shavrukov <max.shavrukov@gmail.com>
2026-09-09 10:01:30 +0300

    включили медиа в продакшн

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,6 +29,11 @@ urlpatterns = [
     path('', include('main.urls', namespace='main')),
     # Путь для CKEditor 5 (важно написать ck_editor_5/)
     path('ck_editor_5/', include('django_ckeditor_5.urls')),
+]
+
+# Добавляем раздачу медиафайлов через serve для продакшена:
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 if settings.DEBUG:
